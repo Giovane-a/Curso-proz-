95d3087ad5fc0b86f348ba2b440b6abc01662694
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2023-08-31 00:56:06 -0300

    .

---
+++ dados.js
@@ -0,0 +1,10 @@
+let nome = ("Luana da silva");
+let cpf = 12345678900;
+let rg = 888888888;
+let altura = 1.60;
+let endereco =(" R. Dr. Bulhões, 553 - Engenho de Dentro, Rio de Janeiro - RJ, 20730-420")
+
+
+
+
+console.log("Meu Nome é "+nome+",Meu Cpf é "+cpf+",Meu Rg é "+rg+",tenho "+altura+" de altura, e moro no seguinte endereço:"+endereco)
