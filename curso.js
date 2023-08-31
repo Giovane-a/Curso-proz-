80e840e97fc22d7c1ae748bee26bcafccaaf9668
Giovane Antonio de paiva <giovane-a.p@hotmail.com>
2023-08-31 00:30:56 -0300

    .

---
+++ curso.js
@@ -0,0 +1,10 @@
+let nome = ("Jose Almeida da silva");
+let cpf = 12345678900;
+let rg = 9517530;
+let altura = 1.78;
+let endereco =("Rua A,380-Centro-Recife/PE")
+
+
+
+
+console.log("Meu Nome é "+nome+",Meu Cpf é "+cpf+",Meu Rg é "+rg+",tenho "+altura+" de altura, e moro no seguinte endereço:"+endereco)
